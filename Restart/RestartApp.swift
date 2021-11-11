1727a192ae686ac7e2dd3d653bfefcff0d080f74
Laure Linn <laure.linn@yahoo.com>
2021-11-11 12:42:20 -0800

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Laure Linn on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
