4c719fddb3d605f55efe2618ea55dc69ae105337
Laure Linn <laure.linn@yahoo.com>
2021-11-11 14:58:32 -0800

    Added home screen

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -17,14 +17,41 @@ struct HomeView: View {
   
   var body: some View {
     VStack(spacing: 20){
-      Text("Home")
-        .font(.largeTitle)
+      // MARK: - HEADER
+      
+      Spacer()
+      
+      Image("character-2")
+        .resizable()
+        .scaledToFit()
+        .padding()
+      
+      // MARK: - CENTER
+      
+      Text("This time thats leads to mastery is dependent on the intensity of our focus.")
+        .font(.title3)
+        .fontWeight(.light)
+        .foregroundColor(.secondary)
+        .multilineTextAlignment(.center)
+        .padding()
+      
+      
+      // MARK: - FOOTER
+      
+      Spacer()
       
       Button(action: {
         isOnboardingViewActive = true
       }) {
+        Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+          .imageScale(.large)
         Text("Restart")
-      }
+          .font(.system(.title3, design: .rounded))
+          .fontWeight(.bold)
+      }//: BUTTON
+      .buttonStyle(.borderedProminent)
+      .buttonBorderShape(.capsule)
+      .controlSize(.large)
     } //: VSTACK
   }
 }
